81529d637f01af1e9e6a1fab4f9f6ddd449242a3
Ajit Mourya <mouryaa33@gmail.com>
2018-02-15 10:23:53 +0530

    updated

--- app/src/main/java/com/example/asus/hackerearth/bbaOne.java
+++ app/src/main/java/com/example/asus/hackerearth/bbaOne.java
@@ -1,13 +1,91 @@
 package com.example.asus.hackerearth;
 
+import android.support.annotation.NonNull;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import com.example.asus.hackerearth.model.DATA;
+import com.example.asus.hackerearth.model.DATAAdapter;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class bbaOne extends AppCompatActivity {
 
+    ListView listView;
+    List<DATA> user;
+    Button button_send;
+    EditText editText_editdata;
+    FirebaseAuth auth = FirebaseAuth.getInstance();
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_bba_one);
+
+        button_send = (Button)findViewById(R.id.send);
+        editText_editdata = (EditText)findViewById(R.id.editdata);
+
+        user = new ArrayList<DATA>();
+        listView = (ListView)findViewById(R.id.List_View);
+
+        getusers();
+
+        button_send.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                DATA data = new DATA();
+                data.setName("infi");
+                data.setMsg(editText_editdata.getText().toString().trim());
+                FirebaseDatabase firebaseDatabase = FirebaseDatabase.getInstance();
+                DatabaseReference myref = firebaseDatabase.getReference().child("INTTWO");
+
+                myref.push().setValue(data).addOnCompleteListener(new OnCompleteListener<Void>() {
+                    @Override
+                    public void onComplete(@NonNull Task<Void> task) {
+                        editText_editdata.setText("");
+                    }
+                });
+            }
+        });
+    }
+
+    public void getusers(){
+        final FirebaseDatabase fd = FirebaseDatabase.getInstance();
+        DatabaseReference myref = fd.getReference().child("INTTWO");
+        myref.addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(DataSnapshot dataSnapshot) {
+                fetchData(dataSnapshot);
+            }
+
+            @Override
+            public void onCancelled(DatabaseError databaseError) {
+
+            }
+        });
+    }
+
+    private void fetchData(DataSnapshot dataSnapshot)
+    {
+        user.clear();
+        for (DataSnapshot ds : dataSnapshot.getChildren())
+        {
+            DATA a = ds.getValue(DATA.class);
+            user.add(a);
+        }
+        DATAAdapter adapter = new DATAAdapter(bbaOne.this,user);
+        listView.setAdapter(adapter);
     }
 }
